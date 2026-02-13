374750d143ff786653c1b6c0eb75bd4673536baa
Jennifer <157560657+jen-exe@users.noreply.github.com>
2026-02-12 23:21:57 -0800

    Add files via upload

---
+++ Homework2/RpnPrinter.java
@@ -0,0 +1,63 @@
+package com.craftinginterpreters.lox;
+
+class RpnPrinter implements Expr.Visitor<String> {
+
+  String print(Expr expr) {
+    return expr.accept(this);
+  }
+
+  @Override
+  public String visitBinaryExpr(Expr.Binary expr) {
+    return expr.left.accept(this) + " "
+        + expr.right.accept(this) + " "
+        + expr.operator.lexeme;
+  }
+
+  @Override
+  public String visitGroupingExpr(Expr.Grouping expr) {
+    return expr.expression.accept(this);
+  }
+
+  @Override
+  public String visitLiteralExpr(Expr.Literal expr) {
+    return expr.value.toString();
+  }
+
+  @Override
+  public String visitUnaryExpr(Expr.Unary expr) {
+    return expr.right.accept(this) + " "
+        + expr.operator.lexeme;
+  }
+
+  @Override public String visitVariableExpr(Expr.Variable expr) { return ""; }
+  @Override public String visitAssignExpr(Expr.Assign expr) { return ""; }
+  @Override public String visitLogicalExpr(Expr.Logical expr) { return ""; }
+  @Override public String visitCallExpr(Expr.Call expr) { return ""; }
+  @Override public String visitGetExpr(Expr.Get expr) { return ""; }
+  @Override public String visitSetExpr(Expr.Set expr) { return ""; }
+  @Override public String visitThisExpr(Expr.This expr) { return ""; }
+  @Override public String visitSuperExpr(Expr.Super expr) { return ""; }
+
+  public static void main(String[] args) {
+    Expr expression = new Expr.Binary(
+      new Expr.Grouping(
+        new Expr.Binary(
+          new Expr.Literal(1),
+          new Token(TokenType.PLUS, "+", null, 1),
+          new Expr.Literal(2)
+        )
+      ),
+      new Token(TokenType.STAR, "*", null, 1),
+      new Expr.Grouping(
+        new Expr.Binary(
+          new Expr.Literal(4),
+          new Token(TokenType.MINUS, "-", null, 1),
+          new Expr.Literal(3)
+        )
+      )
+    );
+
+    System.out.println(new RpnPrinter().print(expression));
+  }
+}
+
